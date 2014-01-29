8b56a384ea83dd8797acaed64d6fbf6c845bd061
Matthew Linder <mattsasa@me.com>
2014-01-29 16:13:31 -0600

    Moved some code around, no real changes

--- JSLab.js
+++ JSLab.js
@@ -1,3 +1,21 @@
 /**
  * Created by gensc004 on 1/28/14.
  */
+
+function changeBackground(newColor) {
+
+    if(newColor == document.body.style.backgroundColor){
+        document.body.style.backgroundColor = "blue";
+    } else {
+        document.body.style.backgroundColor = newColor;
+    }
+}
+
+function eraseText() {
+    document.getElementById("textArea").value = "";
+    }
+
+function displayCharacterCount(){
+    var count = "<p>"  + "here" + "</p>";
+    document.getElementById("character_count").innerHTML = count;
+    }
