d03ca0372ec74bc765b07535722edc2b23430cc2
gensc004 <gensc004@morris.umn.edu>
2014-01-30 13:53:56 -0600

    We have added all of the color and font changes for the text box and have began working on the save, undo, and redo closures.

--- JSLab.js
+++ JSLab.js
@@ -2,20 +2,65 @@
  * Created by gensc004 on 1/28/14.
  */
 
-function changeBackground(newColor) {
-
-    if(newColor == document.body.style.backgroundColor){
-        document.body.style.backgroundColor = "blue";
-    } else {
-        document.body.style.backgroundColor = newColor;
-    }
-}
 
 function eraseText() {
     document.getElementById("textArea").value = "";
+}
+
+function displayCharacterCount(text){
+    var count = "<p>" +"Count: "  + text.length + "</p>";
+    return count;
+}
+function save(text) {
+    var count = 0;
+    var currentSave;
+    var currentSave1;
+    var currentSave2;
+    var currentSave3;
+    var currentSave4;
+    if (count == 0) {
+        currentSave = text;
+        count++;
+    } else if (count == 1) {
+        currentSave1 = text;
+        count++;
+    } else if (count == 2) {
+        currentSave2 = text;
+        count++;
+    } else if (count == 3) {
+        currentSave3 = text;
+        count++;
+    } else if (count == 4) {
+        currentSave4 = text;
+        count++;
     }
 
-function displayCharacterCount(){
-    var count = "<p>"  + "here" + "</p>";
-    document.getElementById("character_count").innerHTML = count;
+    return function undo(newText) {
+        var save = "hello";
+        if (count < 1) {
+            save = currentSave;
+            count--;
+        } else if (count == 2) {
+            save = currentSave1;
+            count--;
+        } else if (count == 3) {
+            save = currentSave2;
+            count--;
+        } else if (count == 4) {
+            save = currentSave3;
+            count--;
+        } else if (count == 5) {
+            save = currentSave4;
+            count--;
+        }
+
+        return save;
     }
+
+}
+
+
+
+
+module.exports.eraseText = eraseText;
+module.exports.displayCharacterCount = displayCharacterCount;
--- Testing.js
+++ Testing.js
@@ -1,3 +1,19 @@
 /**
  * Created by gensc004 on 1/28/14.
  */
+var assert = require("assert");
+var eraseText = require('./JSLab').eraseText;
+var displayCharacterCount = require('./JSLab').displayCharacterCount;
+
+
+describe('Character Count', function(){
+    describe('test 1', function(){
+        it('Displays the character count of a given string', function(){
+            assert.equal(displayCharacterCount("green"), "<p>Count: 5</p>");
+        })
+    });
+
+
+});
+
+
